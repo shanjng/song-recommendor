a55b73974fd03a28e9974485b7a6bf0fd44df209
shanjng <shanjng@umich.edu>
2020-12-19 16:17:21 -0500

    got currently playing track from spotify api!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Login</Link>
+          </li>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          {/* I think it's purposely backwards for longest-prefix matching */}
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <div id="login">
+        <h1>This is an example of the Authorization Code flow</h1>
+        <a href="/login" class="btn btn-primary">Log in with Spotify</a>
+      </div>
+      <div id="loggedin">
+        <div id="user-profile">
+        </div>
+        <div id="oauth">
+        </div>
+        <button class="btn btn-default" id="obtain-new-token">Obtain new token using the refresh token</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+function Home() {
+  return (
+    <div className="App">
+        <div className="App-section section-1">
+          <div className="center-text">
+            Now Playing <br></br>
+            Luis Fonsi - Despacito ft. Daddy Yankee
+          </div>
+        </div>
+        <div className="App-section section-2">
+          <div className="center-text">
+            Recommended <br></br>
+            Silk City, Dua Lipa - Electricity
+          </div>
+        </div>
+    </div>
+  );
+}
